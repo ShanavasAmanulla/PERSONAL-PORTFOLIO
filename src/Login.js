43f75eacca774555a7819bb93fddf988fcdd5177
Shanavas Amanulla <Shanavas.Amanulla@marlabs.com>
2023-03-13 12:41:18 +0530

    Call Fixed

--- src/Login.js
+++ src/Login.js
@@ -3,18 +3,19 @@ import {
   MDBContainer,
   MDBInput,
   MDBCheckbox,
-  MDBBtn,
-  MDBIcon
+
 }
   from 'mdb-react-ui-kit';
 
 import { useNavigate } from 'react-router-dom/dist';
 import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function Login() {
   let navigate = useNavigate();
   const [emailData, SetemailData] = useState([]);
-  const [passData, SetpassData] = useState([]);  
+  const [passData, SetpassData] = useState([]);
 
   function passChange(e) { SetpassData(e.target.value); }
   function emailChange(e) { SetemailData(e.target.value); }
@@ -24,8 +25,7 @@ function Login() {
     navigate('/home');
   }
 
-  function LoginUser() {
-    //localStorage.setItem('userName', 'Guest');
+  function CheckLogin() {
     axios.get('https://localhost:44307/User/CheckLogin', {
       params: {
         emailAddress: emailData,
@@ -35,15 +35,12 @@ function Login() {
       .then(function (response) {
         console.log(response.data);
         if (response.data.userId > 0) {
-          localStorage.setItem('userName', response.data.firstName);
           RouteToHome();
         } else {
-          localStorage.setItem('userName', 'Guest');
           alert('Please check the email and password');
         }
       })
       .catch(function (error) {
-        localStorage.setItem('userName', 'Guest');
         alert(error);
       })
       .finally(function () {
@@ -53,21 +50,15 @@ function Login() {
 
   return (
     <MDBContainer className="p-3 my-5 d-flex flex-column w-50">
-
-      <MDBInput onChange={passChange} wrapperClass='mb-4' label='Email address' id='form1' type='email' />
-      <MDBInput onChange={emailChange} wrapperClass='mb-4' label='Password' id='form2' type='password' />
-
-      <MDBBtn onClick={LoginUser} className="mb-4">Sign in</MDBBtn>
-
-
-
+      <MDBInput onChange={emailChange} wrapperClass='mb-4' label='Email address' id='form1' type='email' />
+      <MDBInput onChange={passChange} wrapperClass='mb-4' label='Password' id='form2' type='password' />
+      <div className='d-flex flex-row justify-content-center mb-4'>
+        <MDBCheckbox name='flexCheck' id='flexCheckDefault' label='I agree all statements in Terms of service'/>
+      </div>
+      <button className="btn btn-primary" onClick={CheckLogin} >Login</button>
       <div className="text-center">
         <p> <a href="#!">Register</a></p>
-        <p></p>
-
-
-
-        </div>
+      </div>
 
     </MDBContainer>
   );
